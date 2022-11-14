e64b49d5ae161a4656446dd8a5dccfd3954cf016
codeguna <pratamagunadhi@gmail.com>
2022-11-14 21:08:42 +0700

    quiz piranti

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(ProfileGuna());
+void main() => runApp(Mahasiswa());
 
 Widget _buildTextSection(
   String text,
@@ -31,18 +31,14 @@ Widget _buildMenuSection(
   );
 }
 
-class ProfileGuna extends StatelessWidget {
+class Mahasiswa extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Widget imageSection = Container(
-      child: Image.asset('images/profile.jpg'),
-    );
-
     Widget titleSection = Container(
-      padding: EdgeInsets.only(top: 16),
+      padding: EdgeInsets.all(15),
       child: Text(
-        'Gunadhi Pratama',
-        textAlign: TextAlign.center,
+        'Status Kehadiran 0%',
+        textAlign: TextAlign.left,
         style: TextStyle(
           fontWeight: FontWeight.bold,
           fontSize: 18,
@@ -53,67 +49,38 @@ class ProfileGuna extends StatelessWidget {
     Widget desriptionSection = Container(
         padding: EdgeInsets.all(16),
         child: Text(
-          '/gu.nad.hi/ adalah Mahasiswa dari Jurusan Sistem Informasi, '
-          '\nMengambil kelas Profesional atau Karyawan dengan kelas 4SIP-01, '
-          '\nMempunyai NRP 220534003',
-          textAlign: TextAlign.center,
+          'Hadir: 0   '
+          'Alpha: 0',
+          textAlign: TextAlign.left,
         ));
 
-    Widget rateSection = Row(
-      children: [
-        Icon(Icons.star, color: Colors.yellow),
-        Icon(Icons.star, color: Colors.yellow),
-        Icon(Icons.star, color: Colors.yellow),
-        Icon(Icons.star, color: Colors.yellow),
-        Icon(Icons.star),
-      ],
-    );
-
-    Widget reviewSection = Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        rateSection,
-        Text('456 Good Reviews about This Person'),
-      ],
-    );
-
-    Widget menuSection = Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        _buildMenuSection(Icons.person, "Nickname", "Guna"),
-        _buildMenuSection(
-            Icons.center_focus_strong_outlined, "NRP", "220534003"),
-        _buildMenuSection(Icons.perm_device_info, "Kelas", "4SIP-1"),
-      ],
-    );
+    Widget listMahasiswa = Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          border: Border.all(),
+        ),
+        child: Text(
+          'Arga Dirgantara',
+          textAlign: TextAlign.left,
+        ));
 
     return MaterialApp(
       title: 'Material Apps',
       home: Scaffold(
           appBar: AppBar(
+            centerTitle: true,
             backgroundColor: Colors.white,
             leading: Icon(Icons.arrow_back_ios, color: Colors.black),
             title: Text(
-              'Profile - Gunadhi Pratama',
+              'Presensi',
               style: TextStyle(color: Colors.black),
             ),
-            actions: <Widget>[
-              Icon(
-                Icons.search,
-                color: Colors.black,
-              )
-            ],
           ),
           body: ListView(
             children: [
-              imageSection,
               titleSection,
               desriptionSection,
-              Container(
-                padding: EdgeInsets.only(bottom: 24),
-                child: reviewSection,
-              ),
-              menuSection,
+              listMahasiswa,
             ],
           )),
     );
